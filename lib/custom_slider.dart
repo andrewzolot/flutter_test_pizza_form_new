c6eb8dd2fd6296a1064cf3fde5067c02d94ad383
andrew <andrew.zolotuev@gmail.com>
2022-01-06 19:38:35 +0700

    Initial commit

---
+++ lib/custom_slider.dart
@@ -0,0 +1,161 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
+import 'package:syncfusion_flutter_core/theme.dart';
+
+final int overlapDistance = -13; // distance of overlaping between text and slider
+
+class CustomSliderWithTextField extends StatefulWidget {
+  const CustomSliderWithTextField({Key? key, required this.sizes, required this.callback}) : super(key: key);
+  final Function(dynamic) callback;
+  final List<double> sizes;
+  @override
+  _CustomSliderState createState() => _CustomSliderState(sizes, callback);
+
+
+}
+
+class _CustomSliderState extends State {
+  TextEditingController txtController  = new TextEditingController();
+  List<double> sizes;
+  double values = 0;
+  Function(dynamic) callback;
+  _CustomSliderState( this.sizes, this.callback){
+    values = sizes[0];
+    txtController.text = values.toString() + " см";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 30.0),
+      child: ColumnSuper(
+        innerDistance: -13,
+        children: [
+          TextField(
+            controller: txtController,
+            textAlign: TextAlign.center,
+            decoration: const InputDecoration(
+              filled: true,
+              fillColor: Color.fromRGBO(236, 239, 241, 1),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(36.0)),
+                borderSide: const BorderSide(color: Colors.transparent, width: 0.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(36.0)),
+                borderSide: const BorderSide(color: Colors.transparent, width: 0.0),
+              ),
+            ),
+          ),
+          SfSliderTheme(
+            data: SfSliderThemeData(
+              overlayRadius: 0,
+            ),
+            child: Container(
+              child: SfSlider(
+                min: sizes[0],
+                max: sizes[1],
+                value: values,
+                stepSize: 10.0,
+                showTicks: false,
+                showLabels: false,
+                enableTooltip: false,
+                thumbShape: _SfThumbShape(),
+                trackShape: CustomTrackShape(),
+                activeColor: Color.fromRGBO(0, 99, 147, 1),
+                inactiveColor: Colors.transparent,
+                onChanged: (value) {
+                  setState(() {
+                    values = value;
+                    txtController.text = values.toString() + " см";
+                    callback(value);
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SfThumbShape extends SfThumbShape {
+  @override
+  void paint(PaintingContext context, Offset center,
+      {required RenderBox parentBox,
+        required RenderBox? child,
+        required SfSliderThemeData themeData,
+        SfRangeValues? currentValues,
+        dynamic currentValue,
+        required Paint? paint,
+        required Animation<double> enableAnimation,
+        required TextDirection textDirection,
+        required SfThumb? thumb}) {
+    final Path path = Path();
+
+    path.moveTo(center.dx, center.dy);
+    path.lineTo(center.dx + 8, center.dy + 10);
+    path.lineTo(center.dx + 8, center.dy + 25);
+    path.lineTo(center.dx - 8, center.dy + 25);
+    path.lineTo(center.dx - 8, center.dy + 10);
+    path.close();
+    context.canvas.drawPath(
+        path,
+        Paint()
+          ..color = themeData.activeTrackColor!
+          ..style = PaintingStyle.fill
+          ..strokeWidth = 2);
+  }
+}
+
+class CustomTrackShape extends SfTrackShape {
+  final left = 12;
+  final right = 12;
+  final height = 5;
+  final delta = 5; // depends of the radius (should be modified in future)
+  @override
+  void paint(PaintingContext context, Offset offset, Offset? thumbCenter,
+      Offset? startThumbCenter, Offset? endThumbCenter,
+      {required RenderBox parentBox,
+        required SfSliderThemeData themeData,
+        SfRangeValues? currentValues,
+        dynamic currentValue,
+        required Animation<double> enableAnimation,
+        required Paint? inactivePaint,
+        required Paint? activePaint,
+        required TextDirection textDirection}) {
+    final Path path = Path();
+    path.moveTo(offset.dx + left, offset.dy);
+    if (thumbCenter!.dx >= offset.dx + left){ //check for position at top right point
+      if (thumbCenter.dx < offset.dx + parentBox.size.width - right - delta){ // if in middle position
+        path.lineTo(thumbCenter.dx, offset.dy);
+        path.lineTo(thumbCenter.dx, offset.dy + height);
+        path.lineTo(offset.dx + left + delta, offset.dy + height);
+        path.moveTo(offset.dx + left, offset.dy);
+      }
+      else{ // in the full right pos
+        path.lineTo(thumbCenter.dx, offset.dy);
+        path.lineTo(thumbCenter.dx - delta, offset.dy + height);
+        path.lineTo(offset.dx + left + delta, offset.dy + height);
+        path.moveTo(offset.dx + left, offset.dy);
+      }
+    }
+    /*
+    else{
+      path.lineTo(offset.dx + left, offset.dy);
+      path.lineTo(offset.dx + left + delta, offset.dy + height);
+      path.moveTo(offset.dx + left, offset.dy);
+    }
+     */
+    path.close();
+    context.canvas.drawPath(
+        path,
+        Paint()
+          ..color = themeData.activeTrackColor!
+          ..style = PaintingStyle.fill
+          ..strokeWidth = 2);
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,353 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'custom_slider.dart';
+
+final List<String> souceNames = ["Острый", "Кисло-сладкий", "Сырный"];
+final pizzaSizesMinAndMax = [30.0, 50.0];
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int testoType = 0;
+  double pizzaSize = pizzaSizesMinAndMax[0];
+  int souceVal = 1;
+  bool needMoreCheese = false;
+  double resultCost = 0;
+  double testoCostPerSq_1 = 1;
+  double testoCostPerSq_2 = 0.7;
+  TextEditingController txtController  = new TextEditingController();
+
+  void calcCost() {
+    if (testoType == 0) resultCost = pi * pow(pizzaSize/2, 2) * testoCostPerSq_1;
+    else resultCost = pi * pow(pizzaSize/2, 2) * testoCostPerSq_2;
+    if (needMoreCheese) resultCost = resultCost + 30;
+    if (souceVal == 1) resultCost = resultCost + 10;
+    else if (souceVal == 2) resultCost = resultCost + 15;
+    else resultCost = resultCost + 40;
+    txtController.text = resultCost.toString() + " руб";
+  }
+
+  _MyHomePageState(){
+    calcCost();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topRight,
+                child: SizedBox(
+                  width: 232,
+                  height: 123,
+                  child:Image(
+                    image: AssetImage('assets/images/pizza.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                        margin: const EdgeInsets.only(top: 33,bottom:9.0),
+                        child: Text(
+                            'Калькулятор пиццы',
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                height: 1.21,
+                                color: Color.fromRGBO(0, 0, 0, 1)
+                            )
+                        ),
+                    ),
+                    Text(
+                        'Выберите параметры:',
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            height: 1.21,
+                            color: Color.fromRGBO(0, 0, 0, 1)
+                        )
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 33,bottom:19.0),
+                      child: FlutterToggleTab(
+                        // width in percent, to set full width just set to 100
+                        width: 90,
+                        borderRadius: 30,
+                        height: 50,
+                        selectedIndex:testoType,
+                        selectedTextStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700),
+                        unSelectedTextStyle: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500),
+                        labels: ["Обычное тесто", "Тонкое тесто"],
+                        selectedLabelIndex: (index) {
+                          setState(() {
+                            testoType = index;
+                          });
+                          calcCost();
+                        },
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Размер:',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 16.0,
+                          height: 1.21,
+                          color: Color.fromRGBO(0, 0, 0, 1),
+                        ),
+                      ),
+                    ),
+                    CustomSliderWithTextField(
+                        sizes: pizzaSizesMinAndMax,
+                        callback: (value) {
+                          pizzaSize = value;
+                          calcCost();
+                        },
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Соус:',
+                        style: TextStyle(
+                          fontFamily: 'i',
+                          fontSize: 16.0,
+                          height: 1.21,
+                          color: Color.fromRGBO(0, 0, 0, 1),
+                        ),
+                      ),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(left: 18.0, right: 11.0, bottom: 10.0),
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children:<Widget>[
+                            ListTile(
+                              contentPadding: EdgeInsets.symmetric(horizontal: 0),
+                              title: Text('Острый'),
+                              trailing: SizedBox(
+                                height: 20, width: 20,
+                                child: Radio(
+                                  value: 1,
+                                  groupValue: souceVal,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      souceVal = value as int;
+                                      calcCost();
+                                    });
+                                  },
+                                  activeColor: Colors.green,
+                                ),
+                              ),
+                            ),
+                            Divider(
+                              color: Color.fromRGBO(232, 232, 232, 1),
+                              height: 1,
+                            ),
+                            ListTile(
+                              contentPadding: EdgeInsets.symmetric(horizontal: 0),
+                              title: Text('Кисло-сладкий'),
+                              trailing: SizedBox(
+                                height: 20, width: 20,
+                                child: Radio(
+                                  value: 2,
+                                  groupValue: souceVal,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      souceVal = value as int;
+                                      calcCost();
+                                    });
+                                  },
+                                  activeColor: Colors.green,
+                                ),
+                              ),
+                            ),
+                            Divider(
+                              color: Color.fromRGBO(232, 232, 232, 1),
+                              height: 1,
+                            ),
+                            ListTile(
+                              contentPadding: EdgeInsets.symmetric(horizontal: 0),
+                              title: Text('Сырный'),
+                              trailing: SizedBox(
+                                height: 20, width: 20,
+                                child: Radio(
+                                  value: 3,
+                                  groupValue: souceVal,
+                                  onChanged: (value) {
+                                    print(value);
+                                    setState(() {
+                                      souceVal = value as int;
+                                      calcCost();
+                                    });
+                                  },
+                                  activeColor: Colors.green,
+                                ),
+                              ),
+                            ),
+                            Divider(
+                              color: Color.fromRGBO(232, 232, 232, 1),
+                              height: 1,
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(top: 18.0),
+                                width: MediaQuery.of(context).size.width,
+                                height: 56.0,
+                                decoration: BoxDecoration(
+                                    color: Color.fromRGBO(240, 240, 240, 1),//remove color to make it transpatent
+                                    border: Border.all(
+                                        style: BorderStyle.solid,
+                                        color: Colors.white),
+                                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                                ),
+                                child: Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: SizedBox(
+                                          width:36,
+                                          height: 34,
+                                          child:SvgPicture.asset(
+                                                "assets/icons/cheese.svg",
+                                                )
+                                          ),
+                                      flex: 1,
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                          "Дополнительный сыр",
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              height: 1.21,
+                                              color: Color.fromRGBO(0, 0, 0, 1)
+                                          )
+                                      ),
+                                      flex: 2,
+                                    ),
+                                    Expanded(
+                                      child: Switch(
+                                        activeColor: Color.fromRGBO(14, 77, 164, 1),
+                                        value: needMoreCheese,
+                                        onChanged: (value) {
+                                          setState(() {
+                                            needMoreCheese = value;
+                                          });
+                                          calcCost();
+                                        },
+                                      ),
+                                      flex: 1,
+                                    ),
+                                  ],
+                                )
+                            ),
+                          ],
+                        ),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Стоимость:',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          height: 1.21,
+                          color: Color.fromRGBO(0, 0, 0, 1),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(bottom:33.0),
+                      child:  TextField(
+                        controller: txtController,
+                        textAlign: TextAlign.center,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromRGBO(236, 239, 241, 1),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(36.0)),
+                            borderSide: const BorderSide(color: Colors.transparent, width: 0.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(left:42.0, top:11.0, right:42.0, bottom:12.0),
+                        child:Text(
+                          "Заказать",
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 16.0,
+                            height: 1.17,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(0, 121, 208, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(22.0),
+                        ),
+                      ),
+                    )
+                  ]
+                ),
+              )
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
